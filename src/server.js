e1db4ffd22b5fbbb1afb042839f97dd12690a1d9
Seung Wook Lee <seungwooklee94@gmail.com>
2020-12-12 05:48:22 +0900

    make it better

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 4100;
 const express = require('express')
 const app = express()
 
-
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"))
@@ -24,7 +23,7 @@ app.post('/submit', function (req, res) {
 		mode: 'text',
 		pythonPath: 'D:/Users/seung/anaconda3/python',
 		pythonOptions: ['-u'], // get print results in real-time 
-		scriptPath: 'D:/Workspace/kohi2020_team3_backend/src',
+		scriptPath: __dirname,
 		args: [JSON.stringify(req.body)]
 	};
 
@@ -38,11 +37,3 @@ app.post('/submit', function (req, res) {
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
-
-
-// PythonShell.run('main.py', options, function (err) {F
-//   if (err) throw err;
-//   console.log('finished');
-// });
-// misc
-//console.log('Hello! Do I need prisma though?\n I just need to run some Python code - no need of database');
